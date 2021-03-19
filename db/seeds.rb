d936616b15538595d0cd8a1c0f543a071f24ed37
koji_yamamoto <koji_ko428@yahoo.co.jp>
2021-03-19 08:10:38 +0000

    favoriteコントローラのシンタックスエラー解消

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# seedにてユーザー作成：ループで３つまとめて作成
+3.times do |index|
+  no = index + 1
+  user = User.create(
+    name:           "user_#{no}",
+    email:  "email_#{no}@example.com",
+    password:               "#{no}password#{no}",
+  )
+  user.save!
+end
