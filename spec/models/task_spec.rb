49b386dff6636f3cc4ccf373bbdbd3212160cb27
kojiko428 <77034582+kojiko428@users.noreply.github.com>
2021-03-19 15:23:38 +0900

    Revert "Step3"

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -5,34 +5,6 @@
 require 'rails_helper'
 
 RSpec.describe 'タスクモデル機能', type: :model do
-  before do
-     @task = FactoryBot.create(:task)
-     @second_task = FactoryBot.create(:second_task)
-
-   end
-  describe '検索機能' do
-    context 'scopeメソッドでタイトルのあいまい検索をした場合' do
-      it "検索キーワードを含むタスクが絞り込まれる" do
-        # title_seachはscopeで提示したタイトル検索用メソッドである。
-        # メソッド名は任意で構わない。
-        expect(Task.search_title('task1').count).to eq 1
-        # expect([, , ]).to include
-
-　　　end
-    end
-    context 'scopeメソッドでステータス検索をした場合' do
-      it "ステータスに完全一致するタスクが絞り込まれる" do
-        # ここに内容を記載する
-        expect(Task.search_status('完了').count).to eq 1
-      end
-    end
-    context 'scopeメソッドでタイトルのあいまい検索とステータス検索をした場合' do
-      it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
-        # ここに内容を記載する
-        expect(Task.search_title('task').search_status('完了').count).to eq 1
-      end
-    end
-  end
 
   describe 'バリデーションのテスト' do
     context 'タスクのタイトルが空の場合' do
