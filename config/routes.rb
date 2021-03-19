28cf76019b30b766cc73c48e65eb2c3f65f078e5
koji_yamamoto <koji_ko428@yahoo.co.jp>
2021-03-19 07:31:39 +0000

    ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'tasks#index'
-  # resources :users
+  resources :users
   resources :tasks do
     collection do
       post :confirm
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   end
 
   # 【1】
-  # resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   # resources :favorites, only: [:create, :destroy]
 end
